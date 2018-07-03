c46e6dbc8a035eaffe16ebd03e113cc6788cae5f
Glen Barnett <gbarnett@jabbyapps.com>
2018-07-03 14:15:36 -0400

    updates

---
+++ L5/app.js
@@ -0,0 +1,23 @@
+// Log to console
+// console.log('Hello World');
+// console.log(123);
+// console.log(true);
+// var greeting = 'Hello';
+// console.log(greeting);
+// console.log([1,2,3,4]);
+// console.log({a:1, b:2});
+console.table({a:1, b:2});
+console.table(["apples", "oranges", "bananas"]);
+
+console.error("oops I shit");
+console.error("this is the real shit");
+console.clear();
+console.warn('Back off bitch');
+console.time('Hello');
+  console.log('Hello World!');
+  console.log('Hello World!');
+  console.log('Hello World!');
+  console.log('Hello World!');
+  console.log('Hello World!');
+
+console.timeEnd('Hello');
---
+++ L6/app.js
@@ -0,0 +1,37 @@
+// var, let, const
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Wonder';
+// console.log(name);
+
+// // Init var
+// var greeting;
+// console.log(greeting);
+// greeting = "FUCK OFF";
+// console.log(greeting);
+
+// var firstName = "Billy";
+
+// let firstName = "Billy";
+// console.log(firstName);
+// let wisdomNight = "see";
+// console.log(wisdomNight);
+
+// const firstName = "Johnny";
+// console.log(firstName);
+// firstName = 'Betty';
+// console.log(firstName);
+
+const person = {
+  name: 'John',
+  age: 30,
+  face: "fugly"
+}
+
+person.name = "Sara Conner";
+console.log(person);
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+numbers.push(6);
+console.log(numbers)
