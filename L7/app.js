7c36f8b972b74b1411c4ca373df86fc77ecedefc
jabbyapps <gbarnett@jabbyapps.com>
2018-07-04 04:40:24 -0400

    lesson updates

--- L7/app.js
+++ L7/app.js
@@ -2,5 +2,28 @@
 
 //String
 
-const name = 'John Doe';
-console.log(typeof name);
+const name = "John Doe";
+
+//Number
+const age = 45;
+//Boolean
+const hasKids = true;
+// Null
+const car = null;
+// Undefined
+let test;
+// Symbol
+const sym = Symbol();
+
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ["movies", "music"];
+// Object literal
+const address = {
+  city: "Fenton",
+  state: "Michigan"
+};
+const today = new Date();
+
+console.log(today);
+console.log(typeof address);
