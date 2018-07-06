23de95b89d93e8ac76eb14ea167c031478cc800a
Glen Barnett <gbarnett@jabbyapps.com>
2018-07-06 12:05:35 -0400

    update

--- L12/app.js
+++ L12/app.js
@@ -1,7 +1,46 @@
 // create some arrays
-const numbers = [1, 2, 3, 4, 5, 6];
+const numbers = [43,56,33,23,44,36,5];
 const numbers2 = new Array(7, 8, 9, 10, 11, 12);
 const fruit = ["apple", "orange", "bananan", "pear"];
 const mixed = [22, "hello", true, undefined, null, { a: 1, b: 2 }, new Date()];
 
-console.log(mixed);
+let val;
+
+// Get array length
+val = numbers.length;
+// Check if its an array
+val = Array.isArray(numbers);
+// Get single value
+val = numbers[2];
+val = numbers[0];
+// insert into array
+numbers[2] = 100;
+// find index of value
+val = numbers.indexOf(100);
+
+// MUTATING ARRAYS
+// Add on to end
+// val = numbers.push(250);
+// // Add to front
+// val = numbers.unshift(25);
+// // remove from end
+// val = numbers.pop();
+// // remove from front
+// val = numbers.shift();
+// // Splice values
+// // numbers.splice(1,4);
+// // Reverse
+// numbers.reverse();
+// // Concatenate array
+// val = numbers.concat(numbers2);
+
+// Sorting arrays
+val = fruit.sort();
+val = numbers.sort();
+
+val = numbers.sort(function(a, b){
+  return a - b;
+})
+
+console.log(numbers);
+console.log(val);
