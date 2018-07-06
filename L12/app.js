810a2959b8594dce0aa82fd6c96baae20709f72f
jabbyapps <gbarnett@jabbyapps.com>
2018-07-06 07:49:57 -0400

    template literals

---
+++ L12/app.js
@@ -0,0 +1,7 @@
+// create some arrays
+const numbers = [1, 2, 3, 4, 5, 6];
+const numbers2 = new Array(7, 8, 9, 10, 11, 12);
+const fruit = ["apple", "orange", "bananan", "pear"];
+const mixed = [22, "hello", true, undefined, null, { a: 1, b: 2 }, new Date()];
+
+console.log(mixed);
